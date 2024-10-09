414adc6f25491ad7efb98cb80b121a3505619837
trevorwrightdev <trevor.wright.710@gmail.com>
2024-10-09 13:57:41 -0700

    Updated camera window size.

--- frontend/app/play/IntroScreen.tsx
+++ frontend/app/play/IntroScreen.tsx
@@ -15,8 +15,8 @@ const IntroScreen:React.FC<IntroScreenProps> = ({ realmName, initialSkin, userna
     return (
         <main className='dark-gradient w-full h-screen flex flex-col items-center pt-28'>
             <h1 className='text-4xl font-semibold'>Welcome to <span className='text-[#CAD8FF]'>{realmName}</span></h1>
-            <section className='flex flex-row mt-32 items-center gap-8'>
-                <div className='aspect-video w-[350px] bg-black rounded-xl border-2 border-[#3F4776]'>
+            <section className='flex flex-row mt-32 items-center gap-24'>
+                <div className='aspect-video w-[337px] h-[227px] bg-black rounded-xl border-2 border-[#3F4776]'>
 
                 </div>
                 <div className='flex flex-col items-center gap-4'>
