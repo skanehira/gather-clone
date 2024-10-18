d8d688f332160f30884b0fe500d847586d35504e
trevorwrightdev <trevor.wright.710@gmail.com>
2024-10-18 15:05:18 -0700

    Added link to github on home page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,18 +1,23 @@
-import AnimatedCharacter from './play/SkinMenu/AnimatedCharacter';
-import Link from 'next/link';
-import BasicButton from '@/components/BasicButton';
+'use client'
+import AnimatedCharacter from './play/SkinMenu/AnimatedCharacter'
+import Link from 'next/link'
+import BasicButton from '@/components/BasicButton'
+import { Code } from '@phosphor-icons/react'
 
-export default async function Index() {
+export default function Index() {
 
   return (
     <div className='w-full grid place-items-center h-screen gradient p-4'>
-        <div className='max-w-[600px]'>
-            <h1 className='font-semibold text-3xl'>Welcome to My Gather Town Clone!</h1>   
-            <p className='w-full text-xl my-6'>
-                This project is a functional recreation of the core features of Gather Town, 
-                built as a portfolio piece to demonstrate my passion for virtual spaces and the technical skills
-                needed to contribute to your team at Gather. 
-            </p>
+        <div className='max-w-[600px] flex flex-col items-center'>
+            <div>
+                <h1 className='font-semibold text-3xl'>Welcome to My Gather Town Clone!</h1>   
+                <p className='w-full text-xl my-6'>
+                    This project is a functional recreation of the core features of Gather Town, 
+                    built as a portfolio piece to demonstrate my passion for virtual spaces and the technical skills
+                    needed to contribute to your team at Gather. 
+                </p>
+            </div>
+            
             <div className='flex flex-row items-center justify-center'>
                 <Link href='/app' >
                     <BasicButton>
@@ -26,6 +31,11 @@ export default async function Index() {
                 </div>
                 <p className='text-sm'>created by <a href="https://www.trevdev.me/" target="_blank" rel="noopener noreferrer" className='font-bold underline'>trevdev</a></p>
             </div>
+            <a href='https://github.com/trevorwrightdev/gather-clone' target="_blank" rel="noopener noreferrer" 
+                className='inline-flex flex-row items-center justify-center gap-2 hover:underline mt-6'>
+                <p className='text-sm'>see the code</p>
+                <Code className='w-4 h-4'/>
+            </a>
         </div>
     </div>
   )
