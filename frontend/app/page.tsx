0d83d674426dc012a5bfefca888d5ac265334a6b
trevorwrightdev <trevor.wright.710@gmail.com>
2024-10-21 23:51:51 -0700

    added a privacy policy

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -5,35 +5,40 @@ import BasicButton from '@/components/BasicButton'
 import { Code } from '@phosphor-icons/react'
 
 export default function Index() {
-
   return (
-    <div className='w-full grid place-items-center h-screen gradient p-4'>
-        <div className='max-w-[600px] flex flex-col items-center'>
-            <div>
-                <h1 className='font-semibold text-3xl'>Welcome to My Gather Clone!</h1>   
-                <p className='w-full text-xl my-6'>
-                    This project is a functional recreation of the core features of Gather, 
-                    built as a portfolio piece to demonstrate my passion for virtual spaces and the technical skills
-                    needed to contribute to your team at Gather. 
-                </p>
-            </div>
-            
-            <div className='flex flex-row items-center justify-center'>
-                <Link href='/app' >
-                    <BasicButton>
-                        Get Started
-                    </BasicButton>
-                </Link>
-            </div>
-            <div className='flex flex-row items-center justify-center mt-6 gap-8'>
-                <p className='text-sm'>created by <a href="https://www.trevdev.me/" target="_blank" rel="noopener noreferrer" className='font-bold underline'>trevdev</a></p>
-                <div className='inline-flex flex-row items-center justify-center gap-2'>
-                    <a href='https://github.com/trevorwrightdev/gather-clone' target="_blank" rel="noopener noreferrer" className='text-sm underline font-bold'>see the code</a>
-                    <Code className='w-4 h-4'/>
-                </div>
-            </div>
-            <AnimatedCharacter src='/sprites/characters/Character_009.png'/>
+    <div className='w-full grid place-items-center h-screen gradient p-4 relative'>
+      <div className='max-w-[600px] flex flex-col items-center'>
+        <div>
+          <h1 className='font-semibold text-3xl'>Welcome to My Gather Clone!</h1>   
+          <p className='w-full text-xl my-6'>
+            This project is a functional recreation of the core features of Gather, 
+            built as a portfolio piece to demonstrate my passion for virtual spaces and the technical skills
+            needed to contribute to your team at Gather. 
+          </p>
         </div>
+        
+        <div className='flex flex-row items-center justify-center'>
+          <Link href='/app' >
+            <BasicButton>
+              Get Started
+            </BasicButton>
+          </Link>
+        </div>
+        <div className='flex flex-row items-center justify-center mt-6 gap-8'>
+          <p className='text-sm'>created by <a href="https://www.trevdev.me/" target="_blank" rel="noopener noreferrer" className='font-bold underline'>trevdev</a></p>
+          <div className='inline-flex flex-row items-center justify-center gap-2'>
+            <a href='https://github.com/trevorwrightdev/gather-clone' target="_blank" rel="noopener noreferrer" className='text-sm underline font-bold'>see the code</a>
+            <Code className='w-4 h-4'/>
+          </div>
+        </div>
+        <AnimatedCharacter src='/sprites/characters/Character_009.png'/>
+      </div>
+      
+      <footer className='absolute bottom-0 w-full text-center pb-8'>
+        <Link href='/privacy-policy' className='text-xs underline font-bold'>
+            privacy policy
+        </Link>
+      </footer>
     </div>
   )
 }
